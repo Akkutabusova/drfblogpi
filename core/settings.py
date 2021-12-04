1e34498f467139b3bf21e6457920f31576ca1e4c
Akku Tabussova <43029682+Akkutabusova@users.noreply.github.com>
2021-12-05 03:11:49 +0600

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -23,8 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.environ.get("SECRET_KEY")
-SECRET_KEY = "apuvicmef^(!j8gx&clu0u(!8m0r^etok^l0)kc!---#(i5=dt"
+SECRET_KEY = os.environ.get("SECRET_KEY")
+# SECRET_KEY = "apuvicmef^(!j8gx&clu0u(!8m0r^etok^l0)kc!---#(i5=dt"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
